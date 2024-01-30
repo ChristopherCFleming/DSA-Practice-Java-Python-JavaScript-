3fd77142d74a9c308ac5a09ec3c34c3efa327630
Christopher C Fleming <63308959+ChristopherCFleming@users.noreply.github.com>
2024-01-30 13:02:18 -0500

    Solve five_sort in Python in linear time and constant space

--- Python/five_sort.py
+++ Python/five_sort.py
@@ -19,3 +19,17 @@
 # five_sort(nums)
 # # twenty-thousand 4s followed by twenty-thousand 5s
 # # -> [4, 4, 4, 4, ..., 5, 5, 5, 5]
+
+
+def five_sort(nums):
+    i = 0
+    j = len(nums) - 1
+
+    while i < j:
+        if nums[j] == 5:
+            j -= 1
+        elif nums[i] == 5:
+            nums[i], nums[j] = nums[j], nums[i]
+        else:
+            i += 1
+    return nums
