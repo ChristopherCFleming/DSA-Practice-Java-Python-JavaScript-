1d080b177fc238db33856d3e32b0c86f19b0720c
Christopher C Fleming <63308959+ChristopherCFleming@users.noreply.github.com>
2025-03-25 11:25:46 -0400

    Solve longestWord in java

---
+++ Java/longestWord.java
@@ -0,0 +1,12 @@
+class Source {
+    public static String longestWord(String sentence) {
+        String[] words = sentence.split(" ");
+        String longest = "";
+
+        for (String word : words) {
+            if (word.length() >= longest.length())
+                longest = word;
+        }
+        return longest;
+    }
+}
--- Java/max_value.java
+++ Java/max_value.java
@@ -11,8 +11,7 @@ class Source {
         return largest;
     }
 
-    public static void run() {
-        // this function behaves as `main()` for the 'run' command
-        // you may sandbox in this function, but should not remove it
-    }
 }
+
+double[] numbers = { 4, 7, 2, 8, 10, 9 };
+Source.maxValue(numbers); // -> 10
